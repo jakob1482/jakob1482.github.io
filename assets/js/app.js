ee4e59eb1c73c46ec1df7646ea9483ede3a4171d
Jakob Jersild Nielsen <jakob.j.nielsen@gmail.com>
2023-10-04 19:04:13 +0200

    Update skill section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,56 @@
+function convertImageToColor(imgElement, hexColor) {
+  // Convert hex to RGB
+  function hexToRgb(hex) {
+    const bigint = parseInt(hex.substring(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    return [r, g, b];
+  }
+
+  const color = hexToRgb(hexColor);
+  
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = imgElement.width;
+  canvas.height = imgElement.height;
+
+  ctx.drawImage(imgElement, 0, 0);
+
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    // If the pixel is not transparent
+    if (data[i + 3] > 0) {
+      data[i]     = color[0]; // Red
+      data[i + 1] = color[1]; // Green
+      data[i + 2] = color[2]; // Blue
+    }
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+
+  // Replace the original image's src with the canvas data
+  imgElement.src = canvas.toDataURL();
+}
+
+function updateSkillIconColors(isDarkTheme) {
+    const hexColor = isDarkTheme ? "#F5F5F5" : "#171717";
+
+    const skillImages = document.querySelectorAll('img[id^="skill_img_"]');
+    skillImages.forEach((img) => {
+        // If images are already loaded:
+        if (img.complete) convertImageToColor(img, hexColor);
+    });
+}
+
+// Initially color the images based on the current theme
+updateSkillIconColors(document.documentElement.classList.contains('dark'));
+
+// When DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
 
   var themeToggleButton = document.getElementById("themeToggle");
@@ -87,4 +140,9 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
+});
+
+// Set up the event listener for theme changes
+document.addEventListener('themeChanged', function(e) {
+  updateSkillIconColors(e.detail.isDark);
 });
