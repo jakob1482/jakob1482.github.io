2c4fb3f3c49380da37682865045704aab1999ec8
Jakob Jersild Nielsen <jakob.j.nielsen@gmail.com>
2024-01-15 13:41:28 +0100

    Refactored app.js and main.css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,10 @@
----
----
-var recognitionsData = {
-  {% for recognition in site.data.recognitions %}{% if recognition.dimensions %}"recognition_{{ forloop.index }}": {{ recognition.dimensions | jsonify }},{% endif %}{% endfor %}
-};
 var {
   OverlayScrollbars,
   ScrollbarsHidingPlugin,
   SizeObserverPlugin,
   ClickScrollPlugin,
 } = OverlayScrollbarsGlobal;
-const $easeInOut = $.bez([0.4,0,0.2,1]);
+const $easeInOut = $.bez([0.4, 0, 0.2, 1]);
 
 // Timers
 var resizeTimerIsotope;
@@ -23,7 +18,7 @@ const eventModalClose = new Event("modal-close");
 
 /**
  * Initializes the scrollbar and sets the theme based on the user's preference.
- * 
+ *
  * @param {HTMLElement} element - The element to initialize the scrollbar on.
  * @returns {OverlayScrollbars} - The initialized OverlayScrollbars instance.
  */
@@ -49,61 +44,57 @@ $(function () {
   // Initialize OverlayScrollbars in the body element.
   var scrollbar = initScrollbar(document.body);
 
-  // Initialize isotope items grid.
-  var $grid = $("[data-isotope-container]").isotope({
-    transitionDuration: 400,
-    resize: false,
-    itemSelector: "[data-isotope-item]",
-    layoutMode: "fitRows",
-    percentPosition: true,
-    fitRows: {
-      isFitWidth: true,
-      columnWidth: "[data-isotope-item]",
-      gutter: 12,
-    },
-  });
-  var skillBoxes = $grid.find("[data-arrange-completed]");
+  if (typeof $grid !== "undefined" && typeof $items !== "undefined") {
+    /**
+     * Event handler for filtering skills in the Isotope grid.
+     * Applies the selected filter to the grid and updates the UI elements.
+     * Temporarily sets the data-arrange-completed attribute to false to prevent 'jumping' animations.
+     */
+    $("ul[role='filterlist']").on("click", "li[role='filter']", function () {
+      // Get the filter value from the clicked element.
+      let filterValue = $(this).attr("data-filter");
+
+      // Set the data-arrange-completed attribute to false to prevent 'jumping' animations.
+      $items.attr("data-arrange-completed", false);
+
+      // Filter the grid while applying a transition
+      $grid.isotope({ filter: filterValue });
+      $(this).siblings().attr("aria-selected", false);
+      $(this).attr("aria-selected", true);
+
+      // Set the data-arrange-completed attribute to true after the transition is complete.
+      clearTimeout(resizeTimerIsotope);
+      resizeTimerIsotope = setTimeout(
+        function () {
+          $items.attr("data-arrange-completed", true);
+        },
+        $("[data-isotope-container]").data("isotope").options[
+          "transitionDuration"
+        ] + 50,
+      );
+    });
+
+    /**
+     * Event handler for window resize event.
+     * Temporarily removes the transition duration, triggers the layout of the grid, and then restores the transition duration.
+     */
+    $(window).on("resize", function () {
+      $grid.isotope({ transitionDuration: 0 });
+      $grid.isotope("layout");
+      $grid.isotope({ transitionDuration: 400 });
+    });
+  }
 
   /**
-   * Event handler for filtering skills in the Isotope grid.
-   * Applies the selected filter to the grid and updates the UI elements.
-   * Temporarily sets the data-arrange-completed attribute to false to prevent 'jumping' animations.
+   * Event handler for the click event on elements with the role "switch".
+   * Triggers the event specified by the data-trigger-event attribute.
+   *
+   * @param {Event} e - The click event object.
    */
-  var resizeTimerIsotope;
-  $("ul[role='filterlist']").on("click", "li", function() {
-    // Get the filter value from the clicked element.
-    let filterValue = $(this).attr("data-filter");
-
-    // Set the data-arrange-completed attribute to false to prevent 'jumping' animations.
-    skillBoxes.attr("data-arrange-completed", false);
-    
-    // Filter the grid while applying a transition
-    $grid.isotope({ filter: filterValue });
-    $(this).siblings().attr("aria-selected", false);
-    $(this).attr("aria-selected", true);
-
-    // Set the data-arrange-completed attribute to true after the transition is complete.
-    clearTimeout(resizeTimerIsotope);
-    resizeTimerIsotope = setTimeout(function() {
-      skillBoxes.attr("data-arrange-completed", true);
-    }, $("[data-isotope-container]").data("isotope").options["transitionDuration"] + 50);
-  });
-
-  /**
-   * Event handler for window resize event.
-   * Temporarily removes the transition duration, triggers the layout of the grid, and then restores the transition duration.
-   */
-  $(window).on("resize", function() {
-    $grid.isotope({ transitionDuration: 0 });
-    $grid.isotope("layout");
-    $grid.isotope({ transitionDuration: 400 });
-  });
-  
-  $("[role='switch']").on("click keydown", function(e) {
-    if (e.type === 'click' || e.key === 'Enter' || e.which === 13) {
+  $("[role='switch']").on("click keydown", function (e) {
+    if (e.type === "click" || e.key === "Enter" || e.which === 13) {
       let triggerEvent = $(this).attr("data-trigger-event");
-      $(this)
-        .trigger(triggerEvent);
+      $(this).trigger(triggerEvent);
     }
   });
 
@@ -113,7 +104,7 @@ $(function () {
    * Disables transitions to ensure instant theme change, then enables transitions after the theme change.
    * If the window width is less than 550px, triggers a click event on the navbar toggle to close the navbar.
    */
-  $("[role='switch']").on("toggle-theme", function() {
+  $("[role='switch']").on("toggle-theme", function () {
     // Disables transitions to ensure instant theme change.
     $("html").addClass("no-transition");
 
@@ -126,12 +117,14 @@ $(function () {
       $("html").addClass("dark");
       localStorage.theme = "dark";
     }
-    
+
     // Updates the UI elements.
-    $(this).attr("aria-checked", !themeIsDark)
+    $(this).attr("aria-checked", !themeIsDark);
 
     // Changes the scrollbar theme.
-    let currentScrollbarTheme = themeIsDark ? "os-theme-light" : "os-theme-dark";
+    let currentScrollbarTheme = themeIsDark
+      ? "os-theme-light"
+      : "os-theme-dark";
     let newScrollbarTheme = themeIsDark ? "os-theme-dark" : "os-theme-light";
     $("." + currentScrollbarTheme).each(function () {
       $(this).removeClass(currentScrollbarTheme).addClass(newScrollbarTheme);
@@ -146,10 +139,14 @@ $(function () {
     }, 200);
   });
 
-  $("[role='switch']").on("toggle-target", function() {
+  /**
+   * Toggles the target element based on the state of the switch.
+   * If the switch is checked, shows the target element, otherwise hides it.
+   */
+  $("[role='switch']").on("toggle-target", function () {
     let targetId = "#" + $(this).attr("aria-controls");
     let target = $(targetId);
-    $(this).attr("aria-checked", $(this).attr("aria-checked") !== "true")
+    $(this).attr("aria-checked", $(this).attr("aria-checked") !== "true");
     $(this).attr("aria-checked") === "true" ? target.show() : target.hide();
   });
 
@@ -161,15 +158,15 @@ $(function () {
     let targetId = "#" + $(this).attr("aria-controls");
     let isExpanded = $(this).attr("aria-expanded") === "true";
     $(this).attr("aria-expanded", !isExpanded);
-    $(this).closest("nav").attr("aria-expanded", !isExpanded);
-    $(targetId).toggle(400, $.bez([0.4,0,0.2,1]));
+    $(this).closest("header").attr("aria-expanded", !isExpanded);
+    $(targetId).toggle(400, $.bez([0.4, 0, 0.2, 1]));
   });
 
   /**
    * Handles the click event on elements with the role "tab-tabs" and attribute "aria-controls".
    * Updates the active tab content and navigation state based on the clicked element.
    */
-  $("[role='sidenav-toggle'][aria-controls]").on("click", function() {
+  $("[role='sidenav-toggle'][aria-controls]").on("click", function () {
     let targetId = "#" + $(this).attr("aria-controls");
     let isExpanded = $(this).attr("aria-expanded") === "true";
     $(targetId).fadeToggle(400, $easeInOut);
@@ -181,7 +178,7 @@ $(function () {
    * Handles the click event on elements with the role "tab-tabs" and attribute "aria-controls".
    * Updates the active tab content and navigation state based on the clicked element.
    */
-  $("[role='tablist-toggle'][aria-controls]").on("click", function() {
+  $("[role='tablist-toggle'][aria-controls]").on("click", function () {
     let targetId = "#" + $(this).attr("aria-controls");
     let isExpanded = $(this).attr("aria-expanded") === "true";
     $(targetId)
@@ -194,11 +191,9 @@ $(function () {
    * Handles the click event on elements with the role "tab" and attribute "aria-controls".
    * Updates the active tab content and navigation state based on the clicked element.
    */
-  $("[role='tab'][aria-controls]").on("click", function() {
+  $("[role='tab'][aria-controls]").on("click", function () {
     // Find the target ID
-    let activeTabs = $(this)
-      .closest("nav")
-      .find("[aria-selected='true']");
+    let activeTabs = $(this).closest("nav").find("[aria-selected='true']");
     let targetId = "#" + $(this).attr("aria-controls");
 
     // First, fade out all tab contents
@@ -221,20 +216,18 @@ $(function () {
    * Handles the click event for elements with the role "modal-open" and the attribute "aria-controls".
    * Opens the modal specified by the value of the aria-controls attribute.
    */
-  $("[role='modal-open'][aria-controls]").on("click", function() {
+  $("[role='modal-open'][aria-controls]").on("click", function () {
     let targetId = "#" + $(this).attr("aria-controls");
     document.querySelector(targetId).showModal();
-    $(targetId)
-      .attr("aria-modal", true)
-      .attr("aria-hidden", false);
-    $(window).trigger('resize');
+    $(targetId).attr("aria-modal", true).attr("aria-hidden", false);
+    $(window).trigger("resize");
   });
 
   /**
    * Handles the click event for elements with the role "modal-close" and the attribute "aria-controls".
    * Closes the modal and updates the accessibility attributes.
    */
-  $("[role='modal-close'][aria-controls]").on("click", function() {
+  $("[role='modal-close'][aria-controls]").on("click", function () {
     let targetId = "#" + $(this).attr("aria-controls");
     $(targetId).trigger("modal-close");
   });
@@ -243,7 +236,7 @@ $(function () {
    * Handles the click event on elements with the role "modal".
    * Closes the corresponding modal and updates the attributes.
    */
-  $("[role='modal']").on("click", function() {
+  $("[role='modal']").on("click", function () {
     let targetId = "#" + $(this).prop("id");
     $(this).trigger("modal-close");
   });
@@ -252,7 +245,7 @@ $(function () {
    * Handles the click event on elements with the role "modal".
    * Closes the corresponding modal and updates the attributes.
    */
-  $("[role='modal']").on("keydown", function(e) {
+  $("[role='modal']").on("keydown", function (e) {
     e.preventDefault();
     let targetId = "#" + $(this).prop("id");
     if (e.key == "Escape") {
@@ -264,13 +257,13 @@ $(function () {
    * Handles the close event on elements with the role "modal".
    * Updates the accessibility attributes.
    */
-  $("[role='modal']").on("modal-close", function(e) {
+  $("[role='modal']").on("modal-close", function (e) {
     let id = "#" + $(this).prop("id");
     $(this)
       .attr("aria-modal", false)
       .attr("aria-hidden", true)
       .attr("tabindex", -1);
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector(id).close();
     }, 300);
   });
@@ -278,10 +271,10 @@ $(function () {
   /**
    * Handles the click event on elements with the role "modal-content".
    * Prevents the click event from propagating further up the DOM tree, ensuring that the modal does not close when the content is clicked.
-   * 
+   *
    * @param {Event} e - The click event object.
    */
-  $("[role='modal-content']").on("click", function(e) {
+  $("[role='modal-content']").on("click", function (e) {
     e.stopPropagation();
   });
 
@@ -290,39 +283,19 @@ $(function () {
    * It adds a class to the modal content element to disable transitions temporarily,
    * clears the resize timer, and then removes the class after a short delay.
    */
-  var resizeTimerModal;
-  $(window).on("resize", function() {
-    let modalContent = $("[open][aria-modal='true']").find("[role='modal-content'");
-    modalContent.addClass('no-transition');
-    
+  $(window).on("resize", function () {
+    let modal = $("[open][aria-modal='true']");
+    let modalContent = modal.find("[role='modal-content'");
+    modalContent.addClass("no-transition");
+
     clearTimeout(resizeTimerModal);
-    resizeTimerModal = setTimeout(function() {
-      modalContent.removeClass('no-transition');
+    resizeTimerModal = setTimeout(function () {
+      modalContent.removeClass("no-transition");
     }, 50);
   });
 
-  /**
-   * Event handler for window resize.
-   * Adjusts the dimensions of posts and translations based on the window size.
-   */
-  $(window).on("resize", function() {
-    $.each(recognitionsData, function(recognitionId, dimensions) {
-      let post = $("#post_" + recognitionId);
-      let translation = $("#translation_" + recognitionId);
-      for (let i = 0; i < dimensions.length; i++) {
-        let dimension = dimensions[i];
-        if (window.innerWidth >= dimension.screen) {
-          post.css("width", dimension.screen - 40 + "px");
-          post.css("height", dimension.post_height + "px");
-          translation.css("height", dimension.translation_height + "px");
-          break; // Exit the loop once a matching condition is found
-        }
-      }
-    });
-  });
-
   // Trigger window resize event 100 milliseconds after the DOMContentLoaded event is triggered
-  setTimeout(function() {
+  setTimeout(function () {
     $(window).trigger("resize");
   }, 100);
 });
