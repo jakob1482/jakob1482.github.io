e8006473d4d922adc80b83073c5c728a93ceca37
Jakob Jersild Nielsen <jakob.j.nielsen@gmail.com>
2024-01-22 19:27:07 +0100

    Add postcss-preset-env plugin and update cssnano configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
+    require("postcss-import"),
     require("tailwindcss"),
+    require("postcss-preset-env"),
     require("autoprefixer"),
-    ...(process.env.JEKYLL_ENV == "production"
-      ? [require("cssnano")({ preset: "default" })]
-      : []),
+    require("cssnano")({ preset: "default" }),
   ],
 };
